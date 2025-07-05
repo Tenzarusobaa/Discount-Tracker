85178b8d2a52b75e8ea9fb8ea4e1601fea072f2b
Tenzarusobaa <alisakili7361@gmail.com>
2025-07-05 23:02:35 +0800

    Services: analytics

---
+++ services/analytics.py
@@ -0,0 +1,9 @@
+#IntelliSense assisted comments
+
+def calculate_average_discount(discounts): # This function calculates the average discount percentage from a list of discounts.
+    if not discounts:
+        return 0.0
+    return sum(d.discount_percent for d in discounts) / len(discounts)
+
+def find_max_discount(discounts): # This function finds the discount with the maximum percentage from a list of discounts.
+    return max(discounts, key=lambda d: d.discount_percent, default=None)
